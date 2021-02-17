be6329a25bee3f8064be78e9761888fce0986a7f
shardul <shardul.aeer@somaiya.edu>
2021-02-17 18:18:09 +0530

    feat: Add express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
 import * as functions from "firebase-functions";
+import * as express from  "express";
+import * as cors from "cors";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('gg');
+})
+
+exports.app = functions.https.onRequest(app);
